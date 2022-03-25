ae01fd488c16088faeb5b8a6ac6695f93cb0d296
DasSuperhirn <31389735+DasSuperhirn@users.noreply.github.com>
2022-03-25 08:55:39 +0100

    Graphics Library implemented into Game class

--- Game.java
+++ Game.java
@@ -1,7 +1,55 @@
-public class Game{
-    
+<<<<<<< HEAD
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+class Surface extends JPanel {
+
+    private void doDrawing(Graphics g) {
+
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawString("Java 2D", 50, 50);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+        doDrawing(g);
+    }
+}
+
+
+public class Game extends JFrame{
+
+    public Game(){
+        initUI();
+    }
+
+    private void initUI() {
+
+        add(new Surface());
+
+        setTitle("Simple Java 2D example");
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
     //Game will run in this class
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+         EventQueue.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                BasicEx ex = new BasicEx();
+                ex.setVisible(true);
+            }
+        });
     }
 }
