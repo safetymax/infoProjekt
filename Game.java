c7c1ad1df605ff9aecf9b2773be332ca8740c4d3
DasSuperhirn <DasSuperhirn@gmx.de>
2022-03-28 08:29:50 +0200

    added Surface class

--- Game.java
+++ Game.java
@@ -1,47 +1,9 @@
-
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
-
-
-class Surface extends JPanel implements Runnable {
-
-    Thread gameThread;
-
-    private void doDrawing(Graphics g) {
-
-        Graphics2D g2d = (Graphics2D) g;
-        g2d.setPaint(Color.PINK);
-        g2d.fillRect(0, 0, 700, 700);
-        g2d.setPaint(Color.BLACK);
-        g2d.drawString("Test 0.1", 350, 350);
-
-    }
-
-    @Override
-    public void paintComponent(Graphics g) {
-
-        super.paintComponent(g);
-        doDrawing(g);
-    }
-
-    public void startGameThread() {
-        gameThread = new Thread(this);
-        gameThread.start();
-    }
-
-    @Override
-    public void run() {
-        while(gameThread!=null) {
-            System.out.println("Game Thread is running");
-        }
-        
-    }
-}
 
 
 public class Game{
---
+++ Surface.java
@@ -0,0 +1,52 @@
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Surface extends JPanel implements Runnable {
+
+    Thread gameThread;
+    int frameCount = 0;
+
+    private void doDrawing(Graphics g) {
+
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setPaint(Color.PINK);
+        g2d.fillRect(0, 0, 700, 700);
+        g2d.setPaint(Color.BLACK);
+        g2d.drawString(""+frameCount, 350, 350);
+        g2d.dispose();
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+        doDrawing(g);
+    }
+
+    public void update() {
+        //update the game
+        frameCount++;
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        while(gameThread!=null) {
+            //game Thread
+            System.out.println("Game Thread is running");
+            //update information
+            update();
+            //draw the screen
+            repaint();
+        }
+        
+    }
+}
