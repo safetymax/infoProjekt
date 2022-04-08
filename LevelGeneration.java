106f4f066b3a3b81f861a2213e70ea0a50a3f834
Aloyminium <mwendel888@gmail.com>
2022-04-08 22:28:01 +0200

    added buggy LevelGeneration pls fix asap

---
+++ LevelGeneration.java
@@ -0,0 +1,76 @@
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LevelGeneration {
+    
+    
+    public static String[] readFile(String levelName) throws FileNotFoundException, IOException{
+        String[] levelInfo = new String[6];
+
+        
+            BufferedReader r = new BufferedReader(new FileReader(levelName));
+
+         
+        
+        for(int i = 0; i < levelInfo.length; i++){
+            
+            try{
+            levelInfo[i] = r.readLine();
+            }catch(Exception e){
+                System.out.println("Error reading file");
+            }
+        
+        }
+        r.close();
+        levelInfo[0] = levelInfo[0].replace("Level: ", "");
+        levelInfo[1] = levelInfo[1].replace("Start X: ", "");
+        levelInfo[2] = levelInfo[2].replace("Start Y: ", "");
+        levelInfo[3] = levelInfo[3].replace("Size: ", "");
+        levelInfo[4] = levelInfo[4].replace("Rows: ", "");
+        levelInfo[5] = levelInfo[5].replace("Colums: ", "");
+        
+       
+        return levelInfo;
+    
+    }
+
+    public static Wall[] generateLevel(String level, int startx, int starty, int size, int rows, int columns) {
+    
+        // du structurer levelInfo:
+                
+        Wall[] walls = new Wall[rows * columns*4]; 
+        //generate Walls from starting x, changes y when x*colums is reached
+        int x = startx;
+        int y = starty;
+        
+        for(int i = 0; i<level.length(); i++){
+            int c = i*4;
+            System.out.println("X: " + x);
+            System.out.println("Y: " + y);
+            System.out.println("Size: "+ size);
+            if(level.charAt(i) == 'W'){
+                walls[c] = new Wall(x,y,x,y+size);  //top left to bot left
+                System.out.println(walls[c].x1);
+                walls[c+1] = new Wall(x,y,x+size,y); //top left to top right
+                walls[c+2] = new Wall(x+size,y,x+size,y+size); //top right to bot right
+                walls[c+3] = new Wall(x,y+size,x+size,y+size); //bot left to bot right
+            }
+            x= x+size;
+            if(x>=startx+size*columns){
+                x = startx;
+                
+                y+=size;
+            }
+        }
+        return walls;
+    
+    }
+}
--- Surface.java
+++ Surface.java
@@ -2,6 +2,10 @@ import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.logging.Level;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -12,7 +16,9 @@ public class Surface extends JPanel implements Runnable {
 
     // Game Elements
     Player player = new Player();
-
+    
+    //Empty level Information
+    String[] info;
     Boundary[] boundaries = new Boundary[256];
 
     int frameCount = 0;
@@ -24,13 +30,30 @@ public class Surface extends JPanel implements Runnable {
         setBackground(Color.BLACK);
 
         //random walls: debug
-        for(int i = 0; i < 6; i++) {
+        /*for(int i = 0; i < 6; i++) {
             boundaries[i] = new Wall((int) (Math.random() * 700), (int) (Math.random() * 700), (int) (Math.random() * 700), (int) (Math.random() * 700));
         }
         boundaries[6] = new Wall(0, 0, 700, 0);
         boundaries[7] = new Wall(0, 0, 0, 700);
         boundaries[8] = new Wall(700, 0, 700, 700);
         boundaries[9] = new Wall(0, 700, 700, 700);
+        */
+        try {
+            info = LevelGeneration.readFile("level1.txt");
+        } catch (FileNotFoundException e) {
+            
+            e.printStackTrace();
+        } catch (IOException e) {
+            
+            e.printStackTrace();
+        }
+        Wall[] walls = LevelGeneration.generateLevel(info[0], Integer.parseInt(info[1]), Integer.parseInt(info[2]), Integer.parseInt(info[3]), Integer.parseInt(info[4]), Integer.parseInt(info[5]));
+        
+        /*for(int i = 0; i < walls.length; i++) {
+            if(walls[i] !=null){
+            System.out.println(walls[i].x1 + " " + walls[i].y1 + " " + walls[i].x2 + " " + walls[i].y2);
+            }
+        }*/
     }
 
     private void doDrawing(Graphics g) {
