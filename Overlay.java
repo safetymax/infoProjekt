1385b47d7fc846cec8b50a61ba6eb3b7d30c40e0
Thorsten der Klappspaten <sailkid12@t-online.de>
2022-04-26 16:20:06 +0200

    Overlay ist jetzt klasse

---
+++ Overlay.java
@@ -0,0 +1,16 @@
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.logging.Level;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class overlay{
+
+    public void draw(Graphics2D g2d, )
+    
+}
