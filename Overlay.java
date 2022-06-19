402a688d4ecd2ed791123985476915960ecd02a0
DasSuperhirn <maximilian.grummt@gmx.de>
2022-06-19 18:05:14 +0200

    Print statements deleted

--- Overlay.java
+++ Overlay.java
@@ -564,61 +564,51 @@ options = true;
             enterA = 6;
             keyH.setKey = true;
             keyH.change[0] = true;
-            System.out.println(31);
         }
         if(enterA == 32){
             enterA = 6;
             keyH.setKey = true;
             keyH.change[2] = true;
-            System.out.println(32);
         }
         if(enterA == 33){
             enterA = 6;
             keyH.setKey = true;
             keyH.change[1] = true;
-            System.out.println(33);
         }
         if(enterA == 34){
             enterA = 6;
             keyH.setKey = true;
             keyH.change[3] = true;
-            System.out.println(34);
         }
         if(enterA == 36){
             enterA = 6;
             keyH.setKey = true;
             keyH.change[4] = true;
-            System.out.println(36);
         }
         if(enterA == 37){
             enterA = 6;
             keyH.setKey = true;
             keyH.change[5] = true;
-            System.out.println(37);
         }
         if(enterA == 38){
             enterA = 6;
             keyH.setKey = true;
             keyH.change[6] = true;
-            System.out.println(38);
         }
         if(enterA == 39){
             enterA = 6;
             keyH.setKey = true;
             keyH.change[7] = true;
-            System.out.println(39);
         }
         if(enterA == 40){
             enterA = 6;
             keyH.setKey = true;
             keyH.change[10] = true;
-            System.out.println(40);
         }
         if(enterA == 41){
             enterA = 6;
             keyH.setKey = true;
             keyH.change[0] = true;
-            System.out.println(41);
         }
         // Sound
         if (enterA == 7) {
