f3a3198a26a0adcce743c5340f357cd0223d7b30
DasSuperhirn <maximilian.grummt@gmx.de>
2022-06-20 18:51:38 +0200

    Boss Sprite Added

--- Player.java
+++ Player.java
@@ -70,6 +70,10 @@ public class Player {
     BufferedImage bullet2Image = null;
     int[][][] bullet2Data = new int[64][64][4];
 
+    File f11 = new File("boss1.png");
+    BufferedImage boss1Image = null;
+    int[][][] boss1Data = new int[64][64][4];
+
     int[][][][] textureData = new int[256][64][64][4];
 
 
@@ -108,7 +112,7 @@ public class Player {
             floorImage = ImageIO.read(f7);
             ceilingImage = ImageIO.read(f8);
             bullet2Image = ImageIO.read(f9);
-
+            boss1Image = ImageIO.read(f11);
         }
         catch(Exception e){
             System.out.println("Error");
@@ -126,6 +130,7 @@ public class Player {
                 floorData[i][j] = floorImage.getData().getPixel(i, j, (int[]) null);
                 ceilingData[i][j] = ceilingImage.getData().getPixel(i, j, (int[]) null);
                 bullet2Data[i][j] = bullet2Image.getData().getPixel(i, j, (int[]) null);
+                boss1Data[i][j] = boss1Image.getData().getPixel(i, j, (int[]) null);
             }
         }
 
@@ -139,6 +144,8 @@ public class Player {
         textureData[7] = floorData;
         textureData[8] = ceilingData;
         textureData[9] = bullet2Data;
+        textureData[10] = null;
+        textureData[11] = boss1Data;
 
 
         
